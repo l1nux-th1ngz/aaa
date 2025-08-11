a7974b092de600a329b0c6b0e971922abc18dff3
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2025-08-10 18:32:38 -0600

    Create go.sh

---
+++ go.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Install Go
+sudo apt -y install golang
+
+# Set environment variables
+echo 'export GOROOT=/usr/lib/go' >> ~/.bashrc
+echo 'export GOPATH=$HOME/Go' >> ~/.bashrc
+echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> ~/.bashrc
+
+# Source the .bashrc file to apply changes permanently
+# Note: This will only affect the current shell session. 
+# To apply changes in new terminal sessions, you need to restart the terminal or run the source command manually.
+source ~/.bashrc
+
+# Create Go workspace directory
+mkdir -p $GOPATH/src
+
+echo "Go has been installed"
