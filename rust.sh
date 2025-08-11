3f3fd590cdb3f4fc371557ba7f5e23b02d6288fe
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2025-08-11 03:34:54 -0600

    Update rust.sh

--- rust.sh
+++ rust.sh
@@ -3,27 +3,56 @@
 # Remove
 sudo apt -y remove --purge rustc
 
-# Install
-sudo apt -y install rustup
-
 # Update
+sudo apt update && sudo apt upgrade -y
+
+# Install
+sudo apt -y install curl build-essential gcc make
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile minimal --default-toolchain nightly
+
+# Update rustup
 rustup self update
 rustup update
 
-# Install toolchains
+# Install
 rustup install stable
 rustup install nightly
 
-# Set default toolchain
+# Set default
 rustup default nightly
 
-# Add rustfmt component
+# Add rustfmt
 rustup component add rustfmt
 
-# Source the cargo environment
-source $HOME/.cargo/env
+# Update PATH
+export PATH="$HOME/.cargo/bin:$PATH"
+source ~/.cargo/env
 
-# Wait :)
+# Create Workspace Directory
+mkdir -p ~/Rust
+
+# Go to the Rust directory
+cd ~/Rust
+
+# Create source file
+cat <<EOF > helloworld.rs
+fn main() {
+    println!("Hello World, this is a test provided by LinuxCapable.com");
+}
+EOF
+
+# Compile
+rustc helloworld.rs
+
+# Run application
+./helloworld
+
+# Update Rust
+rustup update
+
+# Wait
 wait
 
-echo "Rust install complte''
+echo "Rust installation complete"
